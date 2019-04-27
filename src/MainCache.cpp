7eef6f8ac6d14355aad35d810f1196cda1525602
Hao He <hehao1998@pku.edu.cn>
2019-04-27 20:59:15 +0800

    Finished reading trace file

--- src/MainCache.cpp
+++ src/MainCache.cpp
@@ -5,15 +5,25 @@
  */
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdint>
 
 #include "MemoryManager.h"
 #include "Cache.h"
 
+bool parseParameters(int argc, char **argv);
+void printUsage();
+
+const char *traceFilePath;
 MemoryManager *memory = nullptr;
 Cache *cache = nullptr;
 
-
 int main(int argc, char **argv) {
+  if (!parseParameters(argc, argv)) {
+    return -1;
+  }
+
   Cache::Policy l1Policy;
 
   // Initialize memory and cache
@@ -21,7 +31,35 @@ int main(int argc, char **argv) {
   cache = new Cache(memory, l1Policy);
   memory->setCache(cache);
 
+  // Read and execute trace in cache-trace/ folder
+  std::ifstream trace(traceFilePath);
+  if (!trace.is_open()) {
+    printf("Unable to open file %s\n", traceFilePath);
+    return -1;
+  }
+
+  char type; //'r' for read, 'w' for write
+  uint32_t addr;
+  while (trace >> type >> std::hex >> addr) {
+    printf("%c %x\n", type, addr);
+
+    
+  }
+
   delete memory;
   delete cache;
   return 0;
+}
+
+bool parseParameters(int argc, char **argv) {
+  if (argc <= 1) {
+    printUsage();
+    return false;
+  }
+  traceFilePath = argv[1];
+  return true;
+}
+
+void printUsage() {
+  printf("Usage: CacheSim trace-file\n");
 }
